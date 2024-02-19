8f1591348a3b2bbb5f9d7baf8ce9459f632a2186
Victor Gonzalez Morales <victorgm2709@gmail.com>
2024-02-19 14:26:41 +0100

    Legibility

--- sudoku_builder.py
+++ sudoku_builder.py
@@ -1,18 +1,18 @@
 from warnings import filterwarnings
+from sudoku_solver import Sudoku
 import joblib as jl
 import numpy as np
 
-
 def import_model():
   filterwarnings('ignore', category=UserWarning)
-  return jl.load('test_model.joblib')
+  return jl.load('model.joblib')
 
 
 def image_to_number(img: np.ndarray, model) -> int:  
   return int(model.predict([img])[0])
 
 
-def build_sudoku_matrix(cells: list[np.ndarray]) -> list[list[int]]:
+def build_sudoku_matrix(cells: list[np.ndarray]) -> Sudoku:
   model = import_model()
   sudoku = [[-1 for _ in range(9)] for _ in range(9)]
   for index, cell in enumerate(cells):
